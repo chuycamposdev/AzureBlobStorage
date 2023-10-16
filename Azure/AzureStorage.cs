111aa12d6e7b407a4d257e88f8d429b69d70d9da
Jesus Campos <desarrollador01@dalton.com.mx>
2023-10-16 12:23:27 -0500

    Agregar archivos de proyecto.

---
+++ Azure/AzureStorage.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using AzureBlobStorage.Models;
+using Microsoft.Extensions.Options;
+
+namespace AzureBlobStorage.Azure
+{
+    //Low-level module
+    public class AzureStorage
+    {
+        private readonly string _connectionKey;
+        private readonly string _container;
+
+        public AzureStorage(IOptions<AzureSetting> options)
+        {
+            _connectionKey = options.Value.ConnectionKey;
+            _container = options.Value.Container;
+        }
+
+        public async Task<Stream> DownloadBlob(string blobName)
+        {
+            var blobClient = CreateBlobClient(blobName);
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                await blobClient.DownloadToAsync(memoryStream);
+                return memoryStream;
+            }
+        }
+
+        public async Task<BlobContentInfo> UploadBlob(Stream Content, string BlobName)
+        {
+            var blobClient = CreateBlobClient(BlobName);
+            return await blobClient.UploadAsync(Content, true);
+        }
+
+        private BlobClient CreateBlobClient(string blobName)
+        {
+            var serviceClient = new BlobServiceClient(_connectionKey);
+            var containerClient = serviceClient.GetBlobContainerClient(_container);
+            var blobClient = containerClient.GetBlobClient(blobName);
+            return blobClient;
+        }
+    }
+}
---
+++ Services/AzureBlobService.cs
@@ -0,0 +1,24 @@
+﻿using AzureBlobStorage.Azure;
+using AzureBlobStorage.Utils;
+using System;
+
+namespace AzureBlobStorage.Services
+{
+    public class AzureBlobService
+    {
+        private readonly AzureStorage _azureBlob;
+
+        public AzureBlobService(AzureStorage azureBlob)
+        {
+            _azureBlob = azureBlob;
+        }
+
+        public async Task<string> UploadBlobFromContentBase64(string contentBase64)
+        {
+            var streamContent = Utils.Utils.GetStreamFromContentBase64(contentBase64);
+            var blobName = Guid.NewGuid().ToString();
+            var uploadedBlob = await _azureBlob.UploadBlob(streamContent, blobName);
+            return String.Empty;
+        }
+    }
+}
